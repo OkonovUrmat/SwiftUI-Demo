bc1489a4cd68a1295b01ed00bd8115e9d0b25674
OkonovUrmat <okonov.okonov@gmail.com>
2020-05-29 22:08:44 +0600

    Initial commit

---
+++ SwiftUI Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Demo
+//
+//  Created by Urmat on 5/29/20.
+//  Copyright © 2020 Urmat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
