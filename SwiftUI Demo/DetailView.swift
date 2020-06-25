01c43732d8a51066b5d6c01c31701138ed235ddc
OkonovUrmat <okonov.okonov@gmail.com>
2020-06-25 19:36:22 +0600

    Create a Detail View

---
+++ SwiftUI Demo/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  SwiftUI Demo
+//
+//  Created by Urmat on 6/25/20.
+//  Copyright © 2020 Urmat. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("This is a Detail Screen")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
